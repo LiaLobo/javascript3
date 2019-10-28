a5c67523b3dcac4f2648c9ebd005c9a81e720194
LiaLobo <lia.lobo@gmail.com>
2019-10-28 13:34:53 -0300

    resolvido exercícios de revisão a e b

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -1,7 +1,31 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
+// [x] usar Math.random() e Math.floor() ou parseInt()
+
+const pegaBody = document.querySelector('body')
+//OU document.getElementByTagName('body) Porém assim retorna um array
+//OU document.body
+const btn = document.getElementById('trocaCor')
+
+// let teste = document.body.button
+// console.log(teste);  Não funciona
+
+pegaBody.style.backgroundColor = 'purple'
+
+btn.addEventListener('click', clicouBotao)
+
+function geraNum() {
+    const n =parseInt(Math.random() * 255)
+    return n
+}
+
+function clicouBotao() {
+    const r = geraNum()
+    const g = geraNum()
+    const b = geraNum()
+    pegaBody.style.backgroundColor = 'rgb(' + r + ' , ' + g +' , ' + b + ')'
+}
